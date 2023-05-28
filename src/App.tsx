9204cac0a7e9676cc014d81514a44895c30d83b7
Marcos Rolando <marcosrolando.mr@gmail.com>
2023-05-28 19:55:36 -0300

    Remove unused App import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { WorkersScreen } from "./screens/WorkersScreen";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
