f25475eaca42d1aca51d51f1a982db202bd73e76
Ivan Soriano <isoriano@fi.uba.ar>
2023-07-06 17:13:48 -0300

    Add text input for new address

--- src/screens/AddressScreen.tsx
+++ src/screens/AddressScreen.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Avatar, Card, Text } from 'react-native-paper';
+import { Avatar, Card, Text, Button, TextInput } from 'react-native-paper';
 import { View, StyleSheet } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Picker } from '@react-native-picker/picker';
 import { RootStackParamList } from '../utils/navigator';
 import { WorkerDetails } from '../data/worker-details';
@@ -11,11 +12,26 @@ export type AddressScreenProps = {
   details: WorkerDetails;
 };
 
+//TODO: Estas vienen de json server o hardcodeamos?
+// const initialAddresses = [
+//   { label: 'Home', value: 'Matienzo' },
+//   { label: 'Work', value: 'Julian Alvarez' },
+//   { label: 'Uni', value: 'Paseo Colon' },
+// ];
+const initialAddresses = ['Matienzo', 'Paseo Colon', 'Julian Alvarez'];
+
 export const AddressScreen = ({
   route,
 }: NativeStackScreenProps<RootStackParamList, 'AddressScreen'>) => {
   const details = route.params.details;
   const [selectedAddress, setSelectedAddress] = React.useState('');
+  const [addresses, setAddresses] = React.useState(initialAddresses);
+  const [newAddress, setNewAddress] = React.useState('');
+  const onNewAddressPress = () => {
+    setAddresses([...addresses, newAddress]);
+    setNewAddress('');
+  };
+
   return (
     <View style={styles.container}>
       <Card.Content style={styles.professionalCard}>
@@ -23,32 +39,50 @@ export const AddressScreen = ({
         <Text style={styles.nameStyle}>{details.name}</Text>
       </Card.Content>
       <Text style={styles.chooseAddressStyle}>
-        {/* TODO: Centrar este texto */}
         Elija la direccion donde se realizara el servicio
       </Text>
-      <Picker
-        selectedValue={selectedAddress}
-        onValueChange={(itemValue) => setSelectedAddress(itemValue)}
-        mode="dropdown"
-      >
-        <Picker.Item
-          label="Home"
-          value="Home Address"
-          style={styles.pickerItemStyle}
-        />
-        <Picker.Item
-          label="Work"
-          value="Work Address"
-          style={styles.pickerItemStyle}
-        />
-        <Picker.Item
-          label="Parents"
-          value="Parents Adress"
-          style={styles.pickerItemStyle}
-        />
-      </Picker>
 
-      <Text variant="bodyLarge">{selectedAddress}+hola</Text>
+      <View
+        style={{
+          flex: 0,
+          borderWidth: 1,
+          borderRadius: 10,
+        }}
+      >
+        <Picker
+          selectedValue={selectedAddress}
+          onValueChange={(itemValue) => setSelectedAddress(itemValue)}
+          mode="dropdown"
+          style={{ borderColor: 'black' }}
+        >
+          {addresses.map((address, index) => (
+            <Picker.Item
+              key={index}
+              label={address}
+              value={address}
+              style={styles.pickerItemStyle}
+            />
+          ))}
+        </Picker>
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Nueva Direccion"
+          value={newAddress}
+          onChangeText={(text) => setNewAddress(text)}
+          multiline
+          mode="outlined"
+          theme={{
+            colors: { primary: 'black' },
+          }}
+        />
+        {/* TODO: Arreglar como se ve este boton */}
+        <Button style={styles.newAddressButton} onPress={onNewAddressPress}>
+          <MaterialCommunityIcons name="plus" size={20} color={'black'} />
+        </Button>
+      </View>
+      {/* TODO: Agregar botones de Continuar y Cancelar */}
     </View>
   );
 };
@@ -77,24 +111,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginVertical: 5,
   },
-  contactButton: {
-    marginTop: 20,
+  newAddressButton: {
+    marginTop: 8,
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: commonStyle.secondaryColor,
-  },
-  contactText: {
-    fontSize: 20,
-    fontWeight: '300',
-  },
-  basicInfo: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 15,
-  },
-  messageButton: {
-    backgroundColor: commonStyle.cardColor,
-    borderRadius: 50,
+    width: '10%',
   },
   nameStyle: {
     fontSize: 20,
@@ -110,5 +132,18 @@ const styles = StyleSheet.create({
   },
   pickerItemStyle: {
     fontSize: 15,
+  },
+  addressInput: {
+    fontSize: 25,
+  },
+  textInput: {
+    width: '90%',
+  },
+  textInputContainer: {
+    flex: 0,
+    flexDirection: 'row',
+    paddingBottom: 15,
+    paddingLeft: 0,
+    borderColor: 'red',
   },
 });
