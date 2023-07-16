f46d735c4c970f534fc3f5c5ae80119f49e4f612
Ivan Soriano <isoriano@fi.uba.ar>
2023-07-16 19:13:38 -0300

    Return hardcoded categories if request to json server fails

--- src/services/json-server.service.ts
+++ src/services/json-server.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { CategoryType } from '../data/dataTypes';
-
+import * as DefaultCategories from '../data/categories';
 const axiosClient = axios.create({
   baseURL: `https://homesquad-json-server.fly.dev`,
   headers: {
@@ -10,5 +10,11 @@ const axiosClient = axios.create({
 });
 
 export const getCategories = async () => {
-  return (await axiosClient.get<CategoryType[]>('categories')).data;
+  try {
+    const categories = (await axiosClient.get<CategoryType[]>('categories'))
+      .data;
+    return categories;
+  } catch (error) {
+    return DefaultCategories.categories;
+  }
 };
